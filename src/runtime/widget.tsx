1db437f8ed84d7c4b174518a71d280ee981cee70
Steffen.Schmidt <steffen.schmidt@beak.de>
2022-02-20 12:45:57 +0100

    first commit

---
+++ src/runtime/widget.tsx
@@ -0,0 +1,199 @@
+/**
+  Licensing
+
+  Copyright 2022 Esri
+
+  Licensed under the Apache License, Version 2.0 (the "License"); You
+  may not use this file except in compliance with the License. You may
+  obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+  implied. See the License for the specific language governing
+  permissions and limitations under the License.
+
+  A copy of the license is available in the repository's
+  LICENSE file.
+*/
+import { React, jimuHistory, DataSourceComponent, AllWidgetProps, IMState, IMUrlParameters } from 'jimu-core'
+import MapView from 'esri/views/MapView'
+import WebMap from 'esri/WebMap'
+import Extent from 'esri/geometry/Extent'
+import Polygon from 'esri/geometry/Polygon'
+import Graphic from 'esri/Graphic'
+import { MapViewManager, WebMapDataSource, JimuMapViewComponent, JimuMapView } from 'jimu-arcgis'
+
+interface ExtraProps {
+  queryObject: IMUrlParameters
+}
+
+export default class Widget extends React.PureComponent<AllWidgetProps<{}> & ExtraProps, {}> {
+  mapContainer = React.createRef<HTMLDivElement>()
+  mapView: MapView
+  webMap: WebMap
+  extentWatch: __esri.WatchHandle
+  extentWatch2: __esri.WatchHandle
+  mvManager: MapViewManager = MapViewManager.getInstance()
+  graphic: Graphic
+  static mapExtraStateProps = (state: IMState): ExtraProps => {
+    return {
+      queryObject: state.queryObject
+    }
+  }
+
+  onActiveViewChange = (jimuMapView: JimuMapView) => {
+    if (!this.extentWatch2) {
+      this.extentWatch2 = jimuMapView.view.watch('zoom', zoom => {
+        const mainView = jimuMapView.view
+        this.mapView.goTo({
+          center: mainView.center,
+          scale: mainView.scale * 2 * Math.max(mainView.width /
+          this.mapView.width,
+          mainView.height / this.mapView.height)
+        })
+      })
+
+      this.extentWatch2 = jimuMapView.view.watch('extent', extent => {
+        // this.mapView.zoom = (-2)
+        const ring = [
+          [
+            [extent.xmax, extent.ymax],
+            [extent.xmax, extent.ymin],
+            [extent.xmin, extent.ymin],
+            [extent.xmin, extent.ymax]
+          ]
+        ]
+
+        const poly = new Polygon({
+          rings: ring,
+          spatialReference: { wkid: 102100 }
+        })
+
+        const symbol = {
+          type: 'simple-fill',
+          color: [51, 51, 204, 0.4],
+          style: 'solid',
+          outline: {
+            color: 'red',
+            width: 1
+          }
+        }
+        this.mapView.graphics.remove(this.graphic)
+
+        this.graphic = new Graphic({
+          geometry: poly,
+          symbol: symbol
+        })
+
+        this.mapView.graphics.add(this.graphic)
+      })
+    }
+  }
+
+  stopEvtPropagation = (event: Event) => {
+    event.stopPropagation()
+  }
+
+  disableZooming = (view: MapView) => {
+    view.popup.dockEnabled = true
+
+    view.ui.components = ['attribution']
+
+    // disable mouse wheel scroll zooming on the view
+    view.on('mouse-wheel', this.stopEvtPropagation)
+
+    // disable zooming via double-click on the view
+    view.on('double-click', this.stopEvtPropagation)
+
+    // disable zooming out via double-click + Control on the view
+    view.on('double-click', ['Control'], this.stopEvtPropagation)
+
+    // disables pinch-zoom and panning on the view
+    view.on('drag', this.stopEvtPropagation)
+
+    // disable the view's zoom box to prevent the Shift + drag
+    // and Shift + Control + drag zoom gestures.
+    view.on('drag', ['Shift'], this.stopEvtPropagation)
+    view.on('drag', ['Shift', 'Control'], this.stopEvtPropagation)
+
+    // prevents zooming with the + and - keys
+    view.on('key-down', (event) => {
+      const prohibitedKeys = [
+        '+',
+        '-',
+        'Shift',
+        '_',
+        '=',
+        'ArrowUp',
+        'ArrowDown',
+        'ArrowRight',
+        'ArrowLeft'
+      ]
+      const keyPressed = event.key
+      if (prohibitedKeys.includes(keyPressed)) {
+        event.stopPropagation()
+      }
+    })
+
+    return view
+  }
+
+  onDsCreated = (webmapDs: WebMapDataSource) => {
+    if (!webmapDs) {
+      return
+    }
+
+    if (!this.mvManager.getJimuMapViewById(this.props.id)) {
+      const options: __esri.MapViewProperties = {
+        map: webmapDs.map,
+        container: this.mapContainer.current
+      }
+      if (this.props.queryObject?.[this.props.id]) {
+        const extentStr = this.props.queryObject[this.props.id].substr('extent='.length)
+        let extent
+        try {
+          extent = new Extent(JSON.parse(extentStr))
+        } catch (err) {
+          console.error('Bad extent URL parameter.')
+        }
+
+        if (extent) {
+          options.extent = extent
+        }
+      }
+      this.mapView = new MapView(options)
+      this.mapView.ui.components = ['attribution']
+      this.disableZooming(this.mapView)
+      this.mvManager.createJimuMapView({
+        mapWidgetId: this.props.id,
+        view: this.mapView,
+        dataSourceId: webmapDs.id,
+        isActive: true
+      }).then(jimuMapView => {
+        if (!this.extentWatch) {
+          this.extentWatch = jimuMapView.view.watch('extent', (extent: __esri.Extent) => {
+            jimuHistory.changeQueryObject({
+              [this.props.id]: `extent=${JSON.stringify(extent.toJSON())}`
+            })
+          })
+        }
+      })
+    }
+  }
+
+  mapNode = <div className="widget-map" style={{ width: '100%', height: '100%' }} ref={this.mapContainer}></div>
+
+  render () {
+    if (!this.props.useDataSources || this.props.useDataSources.length === 0) {
+      return 'Select a webmap in the settings panel'
+    }
+    return <div className="overview-map-view" style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+        <DataSourceComponent useDataSource={this.props.useDataSources[0]} onDataSourceCreated={this.onDsCreated}>
+            {this.mapNode}
+        </DataSourceComponent>
+        <JimuMapViewComponent useMapWidgetId={this.props.useMapWidgetIds?.[0]} onActiveViewChange={this.onActiveViewChange}></JimuMapViewComponent>
+        </div>
+  }
+}
---
+++ src/setting/setting.tsx
@@ -0,0 +1,43 @@
+
+import { React, Immutable, UseDataSource, DataSourceManager } from 'jimu-core'
+import { AllWidgetSettingProps } from 'jimu-for-builder'
+import { DataSourceTypes, ArcGISDataSourceTypes } from 'jimu-arcgis'
+import { DataSourceSelector } from 'jimu-ui/advanced/data-source-selector'
+import { SettingSection, SettingRow, MapWidgetSelector } from 'jimu-ui/advanced/setting-components'
+
+export default class Setting extends React.PureComponent<AllWidgetSettingProps<{}>, {}> {
+  // supportedTypes = Immutable([DataSourceTypes.WebMap]);
+
+  onDataSourceSelected = (useDataSources: UseDataSource[]) => {
+    this.props.onSettingChange({
+      id: this.props.id,
+      useDataSources: useDataSources
+    })
+  }
+
+  onMapSelected = (useMapWidgetIds: string[]) => {
+    this.props.onSettingChange({
+      id: this.props.id,
+      useMapWidgetIds: useMapWidgetIds
+    })
+  }
+
+  supportedTypes = Immutable([ArcGISDataSourceTypes.WebMap])
+  dsManager = DataSourceManager.getInstance()
+
+  render () {
+    return <div className="overview-map-setting p-2">
+      <div>Select the main map</div>
+      <DataSourceSelector
+        types={ Immutable([DataSourceTypes.WebMap])}
+        mustUseDataSource
+        useDataSources={this.props.useDataSources}
+        onChange={this.onDataSourceSelected}
+        widgetId={this.props.id}
+      />
+      <div className="source-descript text-break">Select the map for your overviewmap</div>
+      <MapWidgetSelector onSelect={this.onMapSelected} useMapWidgetIds={this.props.useMapWidgetIds}/>
+
+    </div>
+  }
+}
